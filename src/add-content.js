09b22407c440c4e1fed92bbd6cf6e2e947acb5c1
duruomeng <du_ruomeng@163.com>
2020-05-28 15:12:45 +0800

    webpack

---
+++ src/add-content.js
@@ -0,0 +1,3 @@
+export default function(){
+  document.write('using npm scripts hhhhhhhhhh')
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import addContent from './add-content.js'
+document.write('My first webpack add.<br/>')
+addContent()
